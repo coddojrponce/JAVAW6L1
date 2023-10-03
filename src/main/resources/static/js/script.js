c09677890eb592dc92a717213f15a3d8c399a2bb
Coding w Rob <rponce@codingdojo.com>
2023-10-03 17:38:26 -0500

    first commit

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,16 @@
+
+let loginForm = document.getElementById("login-form");
+
+let registerForm = document.getElementById("register-form")
+
+console.log("hello")
+
+const login = ()=>{
+	registerForm.classList.add("hide")
+	loginForm.classList.remove("hide")
+}
+
+const register = ()=>{
+	registerForm.classList.remove("hide")
+	loginForm.classList.add("hide")
+}
